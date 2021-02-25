7447421e3f32dbc8f23ab2c4c2457dcf874ff56a
upgradedavid <davi.vuko@gmail.com>
2021-02-25 14:35:10 +0100

    Now I reverted back to a past stage.

--- myfirsttest.R
+++ myfirsttest.R
@@ -1,11 +1,4 @@
 print("myfirsttest")
 addedsomeline <- c()
-<<<<<<< HEAD
 # Well, let's see what happens nex
 #######t
-=======
-
-# oh wow this is gonna mess up my system, as I have commited changes that have not been pushed yet, while this one definitely will.
->>>>>>> 6bcf6023597cde45f06e7a1adffa0305ad997fe1
-
-# OK, I have now seen what's different and added a new line.
